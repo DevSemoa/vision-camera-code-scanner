ac7b1727adb1d3e428c8d434ec180879e0108710
Herve EKPE <herve.ekpe@semoa-togo.com>
2023-09-11 14:10:07 +0000

    minor fix

--- android/src/main/java/com/visioncameracodescanner/VisionCameraCodeScannerPluginPackage.java
+++ android/src/main/java/com/visioncameracodescanner/VisionCameraCodeScannerPluginPackage.java
@@ -7,7 +7,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.mrousavy.camera.frameprocessor.FrameProcessorPlugin;
-import com.mrousavy.camera.frameprocessor.FrameProcessorPluginRegistry
+import com.mrousavy.camera.frameprocessor.FrameProcessorPluginRegistry;
 
 import java.util.Collections;
 import java.util.List;
